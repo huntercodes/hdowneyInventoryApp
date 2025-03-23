07a7e49f1b0a7baa6b1ee4823e5cc72e9c7c771d
hunter downey <hdowneymac@gmail.com>
2025-03-23 17:08:59 -0500

    inital commit - spring boot app setup

---
+++ src/main/java/com/hunterdowney/hdowneyinventoryapp/HdowneyInventoryAppApplication.java
@@ -0,0 +1,13 @@
+package com.hunterdowney.hdowneyinventoryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdowneyInventoryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdowneyInventoryAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hunterdowney/hdowneyinventoryapp/HdowneyInventoryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hunterdowney.hdowneyinventoryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdowneyInventoryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
